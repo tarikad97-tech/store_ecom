90d3c76d65d87f5136964e751fcd3394e82214c1
AD2004 <elkoubaiadnane2004@gmail.com>
2026-02-13 10:22:02 +0100

    add send.php

--- contact.php
+++ contact.php
@@ -56,9 +56,9 @@ include 'navbar.php';
                         </div>
                         <div class="col-lg-7">
                             <form action="php/sendContact.php" class="" method="post">
-                                <input type="text" class="w-100 form-control border-0 py-3 mb-4" placeholder="Your Name">
-                                <input type="email" class="w-100 form-control border-0 py-3 mb-4" placeholder="Enter Your Email">
-                                <textarea class="w-100 form-control border-0 mb-4" rows="5" cols="10" placeholder="Your Message"></textarea>
+                                <input type="text" class="w-100 form-control border-0 py-3 mb-4" name="fname" placeholder="Your Name">
+                                <input type="email" class="w-100 form-control border-0 py-3 mb-4" name="email" placeholder="Enter Your Email">
+                                <textarea class="w-100 form-control border-0 mb-4" rows="5" cols="10" name="messsage" placeholder="Your Message"></textarea>
                                 <button class="w-100 btn form-control border-secondary py-3 bg-white text-primary " type="submit">Submit</button>
                             </form>
                         </div>
---
+++ php/send.php
@@ -0,0 +1,43 @@
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'vendor/autoload.php';
+
+$mail = new PHPMailer(true);
+$fname=$_POST['fname'];
+$email=$_POST['email'];
+
+$message=$_POST['message'];
+try {
+    // Configuration SMTP
+    $mail->isSMTP();
+    $mail->Host       = 'smtp.gmail.com'; 
+    $mail->SMTPAuth   = true;
+    $mail->Username   = 'elkoubaiadnane2004@gmail.com';
+    $mail->Password   = 'axyn tuwt lqsm ykoz';
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+    $mail->Port       = 587;
+
+    // Expéditeur
+    $mail->setFrom($email, $fname);
+
+    // Destinataire
+    $mail->addAddress('elkoubaiadnane2004@gmail.com', $fname);
+
+    // Contenu
+    $mail->isHTML(true);
+    
+    $mail->Body    = $message;
+    $mail->AltBody = 'Email envoyé avec PHPMailer';
+
+    $mail->send();
+
+    header('location: ./');
+} catch (Exception $e) {
+    echo "Erreur : {$mail->ErrorInfo}";
+}
+?>
+
+
+<!-- composer require phpmailer/phpmailer -->
