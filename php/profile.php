c9d110920e1ae2152e4e58b51a5c5836a201f92d
AD2004 <elkoubaiadnane2004@gmail.com>
2026-02-13 16:02:45 +0100

    last update by adnane

---
+++ php/profile.php
@@ -0,0 +1,26 @@
+<?php
+session_start();
+require_once 'config.php';
+// if (!isset($_SESSION['id_cl'])) {
+//     header('Location: ../login.php');
+//     exit();
+// }
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>page profile</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+
+</head>
+<body>
+    
+       <h1 class="text-center">welcome to your prfile</h1>
+
+
+    
+</body>
+</html>
