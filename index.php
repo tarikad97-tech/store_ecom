de0ba455c27e58508a47d8eacbcb34e2120df105
SAAD DAHMANE <69207091+saaddhm@users.noreply.github.com>
2026-02-13 10:53:15 +0100

    changed style image products

--- index.php
+++ index.php
@@ -175,7 +175,7 @@ while ($row = mysqli_fetch_assoc($res)) {
     <div class="rounded position-relative fruite-item">
         <div class="fruite-img">
             <img src="<?php echo $images; ?>" 
-                 class="img-fluid w-100 rounded-top" alt="">
+                 class="img-fluid w-100 rounded-top" alt="" style="max-height: 250px; min-height: 250px; object-fit: cover;">
         </div>
 
         <div class="text-white bg-secondary px-3 py-1 rounded position-absolute" 
