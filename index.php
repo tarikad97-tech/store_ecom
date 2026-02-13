02eb72b216d8206b16d90cd1de677c12878f2458
tarik <tarikad97@gmail.com>
2026-02-13 10:37:17 +0100

    index.php update image product

--- index.php
+++ index.php
@@ -160,33 +160,50 @@ while ($row=mysqli_fetch_assoc($res)) {
                             <div class="row g-4">
                                 <div class="col-lg-12">
                                     <div class="row g-4">
+<?php
+$sql = "SELECT * FROM produit";
+$res = mysqli_query($db, $sql);
 
-                   <?php 
-              
-              
-              
-                    $sql = "SELECT * FROM produit";
-                    $res = mysqli_query($db, $sql);
-                    while ($row = mysqli_fetch_assoc($res)) {
-                 $images = !empty($row['imgpr_pr']) ? $row['imgpr_pr'] : 'https://via.placeholder.com/300';
-                         ?>                     
-                                        <div class="col-md-6 col-lg-4 col-xl-3">
-                                            <div class="rounded position-relative fruite-item">
-                                                <div class="fruite-img">
-                                                    <img src="img/fruite-item-5.jpg" class="img-fluid w-100 rounded-top" alt="">
-                                                </div>
-                                                <div class="text-white bg-secondary px-3 py-1 rounded position-absolute" style="top: 10px; left: 10px;">Fruits</div>
-                                                <div class="p-4 border border-secondary border-top-0 rounded-bottom">
-                                                    <h4><?php echo htmlspecialchars($row['nom_pr']); ?></h4>
-                                                    <p><?php echo htmlspecialchars($row['desc_pr']); ?></p>
-                                                    <div class="d-flex justify-content-between flex-lg-wrap">
-                                                        <p class="text-dark fs-5 fw-bold mb-0">  $<?php echo number_format($row['prix_pr'], 2); ?>   / kg</p>
-                                                        <a href="#" class="btn border border-secondary rounded-pill px-3 text-primary"><i class="fa fa-shopping-bag me-2 text-primary"></i> Add to cart</a>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>  
-                                        <?php } ?>                         
+while ($row = mysqli_fetch_assoc($res)) {
+
+    $images = !empty($row['imgpr_pr']) 
+        ? "uploads/" . $row['imgpr_pr'] 
+        : "https://via.placeholder.com/300";
+?>
+                                        
+<div class="col-md-6 col-lg-4 col-xl-3">
+    <div class="rounded position-relative fruite-item">
+        <div class="fruite-img">
+            <img src="<?php echo $images; ?>" 
+                 class="img-fluid w-100 rounded-top" alt="">
+        </div>
+
+        <div class="text-white bg-secondary px-3 py-1 rounded position-absolute" 
+             style="top: 10px; left: 10px;">
+             Fruits
+        </div>
+
+        <div class="p-4 border border-secondary border-top-0 rounded-bottom">
+            <h4><?php echo htmlspecialchars($row['nom_pr']); ?></h4>
+
+            <p><?php echo htmlspecialchars($row['desc_pr']); ?></p>
+
+            <div class="d-flex justify-content-between flex-lg-wrap">
+                <p class="text-dark fs-5 fw-bold mb-0">
+                    $<?php echo number_format($row['prix_pr'], 2); ?> / kg
+                </p>
+
+                <a href="add_to_cart.php?id=<?php echo $row['id_pr']; ?>" 
+                   class="btn border border-secondary rounded-pill px-3 text-primary">
+                   <i class="fa fa-shopping-bag me-2 text-primary"></i> 
+                   Add to cart
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php } ?>                     
                                     </div>
                                 </div>
                             </div>
